Bootleggers-BrokenLab/packages_apps_Launcher3-old

Launcher3: Allow to tweak blur radius on all apps and on overview
Change-Id: I724cf8eeab40c915009b36f030b0a79788f749e2

src/com/android/launcher3/settings/SettingsMisc.java
@@ -36,6 +36,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.bootleggers.BootlegUtils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -181,6 +183,11 @@ public class SettingsMisc extends FragmentActivity
                     // Show if plugins are enabled or flag UI is enabled.
                     return FeatureFlags.showFlagTogglerUi(getContext()) ||
                             PluginManagerWrapper.hasPlugins(getContext());
+
+                case Utilities.KEY_ALLOW_OVERVIEW_BLUR:
+                case Utilities.KEY_OVERVIEW_BLUR:
+                    return BootlegUtils.supportsBlur();
+
             }
             return true;
         }

Project-Awaken/android_packages_apps_Launcher3

src/com/android/launcher3/settings/SettingsMisc.java
@@ -36,6 +36,8 @@ import androidx.preference.PreferenceGroup.PreferencePositionCallback;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.internal.util.awaken.Utils;
+
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.R;
@@ -181,6 +183,11 @@ public class SettingsMisc extends FragmentActivity
                     // Show if plugins are enabled or flag UI is enabled.
                     return FeatureFlags.showFlagTogglerUi(getContext()) ||
                             PluginManagerWrapper.hasPlugins(getContext());
+
+                case Utilities.KEY_ALLOW_OVERVIEW_BLUR:
+                case Utilities.KEY_OVERVIEW_BLUR:
+                    return Utils.supportsBlur();
+
             }
             return true;
         }