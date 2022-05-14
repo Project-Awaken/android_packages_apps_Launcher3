653256210ffc4647b6ceb53f149c8e971bdceca3
Ratoriku <a1063021545@gmail.com>
2022-05-14 20:07:48 +0800

    Launcher3: Add toggle to use dark status bar on Home screen
    * Since top shadow is allowed to be disabled, use dark
      status bar is a good idea if a light wallpaper is in
      current.
    
    * Inspired from lawnchair:
      https://github.com/LawnchairLauncher/lawnchair/commit/fd2de2173fbbb8b2f349a51352791432da429d90#
    
    Co-authored-by: Suphon Thanakornpakapong <paphonb@gmail.com>
    Change-Id: I682fc4ba36415564086927ae1264e33c012fd28f

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -244,7 +244,7 @@ import java.util.stream.Stream;
  */
 public class Launcher extends StatefulActivity<LauncherState>
         implements LauncherExterns, Callbacks, InvariantDeviceProfile.OnIDPChangeListener,
-        PluginListener<LauncherOverlayPlugin> {
+        PluginListener<LauncherOverlayPlugin>, SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String TAG = "Launcher";
 
     public static final ActivityTracker<Launcher> ACTIVITY_TRACKER = new ActivityTracker<>();
@@ -296,6 +296,8 @@ public class Launcher extends StatefulActivity<LauncherState>
     private StateManager<LauncherState> mStateManager;
 
     private static final int ON_ACTIVITY_RESULT_ANIMATION_DELAY = 500;
+
+    private static final String KEY_DARK_STATUS_BAR = "pref_dark_status_bar";
 
     // How long to wait before the new-shortcut animation automatically pans the workspace
     @VisibleForTesting public static final int NEW_APPS_PAGE_MOVE_DELAY = 500;
@@ -545,7 +547,10 @@ public class Launcher extends StatefulActivity<LauncherState>
         registerReceiver(mScreenOffReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
 
         getSystemUiController().updateUiState(SystemUiController.UI_STATE_BASE_WINDOW,
-                Themes.getAttrBoolean(this, R.attr.isWorkspaceDarkText));
+                Themes.getAttrBoolean(this, R.attr.isWorkspaceDarkText)
+                || mSharedPrefs.getBoolean(KEY_DARK_STATUS_BAR, false));
+
+        mSharedPrefs.registerOnSharedPreferenceChangeListener(this);
 
         if (mLauncherCallbacks != null) {
             mLauncherCallbacks.onCreate(savedInstanceState);
@@ -577,6 +582,13 @@ public class Launcher extends StatefulActivity<LauncherState>
 
     public OnboardingPrefs<? extends Launcher> getOnboardingPrefs() {
         return mOnboardingPrefs;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences SharedPrefs, String key) {
+        if (key.equals(KEY_DARK_STATUS_BAR)) {
+            recreate();
+        }
     }
 
     @Override
